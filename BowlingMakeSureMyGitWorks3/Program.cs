67b7cb148b357224143ef38c03059709afbc6670
colin <colingdegroat@gmail.com>
2021-11-01 17:48:01 -0400

    Add project files.

---
+++ BowlingMakeSureMyGitWorks3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BowlingMakeSureMyGitWorks3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
